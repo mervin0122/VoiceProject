39d6661fd1ec0af6762e9affdd91fcb2812aeca1
mervin0122 <269056581@qq.com>
2018-08-01 17:42:48 +0800

    11

--- VoiceProject/src/main/java/com/cn/mcc/utils/voice/udp/UDPReceiver.java
+++ VoiceProject/src/main/java/com/cn/mcc/utils/voice/udp/UDPReceiver.java
@@ -12,69 +12,60 @@ public class UDPReceiver{
     FileOutputStream fileWriter;
     int bytesReceived, bytesToReceive;
 	    
-    public UDPReceiver(int port) throws IOException
-    {
+    public UDPReceiver(int port) throws IOException {
 	// Init stuff
-	s = new DatagramSocket(port);
-	buffer = new byte[2560];
-	
-	System.out.println(" -- Ready to receive file on port: "+port);
+		s = new DatagramSocket(port);
+		buffer = new byte[2560];
+		System.out.println(" -- Ready to receive file on port: "+port);
 		
 	
 	// 1. Wait for a sender to transmit the filename
-	initPacket = receivePacket();
+		initPacket = receivePacket();
 	
-	initString = "Recieved-"+new String(initPacket.getData(), 0, initPacket.getLength());
-	StringTokenizer t = new StringTokenizer(initString, "::");
-	filename = t.nextToken();
-	bytesToReceive = new Integer(t.nextToken()).intValue();
+		initString = "Recieved-"+new String(initPacket.getData(), 0, initPacket.getLength());
+		StringTokenizer t = new StringTokenizer(initString, "::");
+		filename = t.nextToken();
+		bytesToReceive = new Integer(t.nextToken()).intValue();
 	
-	System.out.println("  -- The file will be saved as: "+filename);
-	System.out.println("  -- Expecting to receive: "+bytesToReceive+" bytes");
+		System.out.println("  -- The file will be saved as: "+filename);
+		System.out.println("  -- Expecting to receive: "+bytesToReceive+" bytes");
 	
 	// 2. Send an reply containing OK to the sender
-	send(initPacket.getAddress(), initPacket.getPort(), (new String("OK")).getBytes());
+		send(initPacket.getAddress(), initPacket.getPort(), (new String("OK")).getBytes());
 	//System.out.println("send something to port: "+initPacket.getPort());
 	
 	
 	// 3. Receive the contents of the file
-	fileWriter = new FileOutputStream(filename);
+		fileWriter = new FileOutputStream(filename);
 	
-	while(bytesReceived < bytesToReceive)
-	    {
+	while(bytesReceived < bytesToReceive) {
 		receivedPacket = receivePacket();
 		fileWriter.write(receivedPacket.getData(), 0,  receivedPacket.getLength());
 		bytesReceived = bytesReceived + receivedPacket.getLength();
-	    }
-	System.out.println("  -- File transfer complete.");
+	}
+		System.out.println("  -- File transfer complete.");
     }
     
     
     public DatagramPacket receivePacket() throws IOException{
 	
-	DatagramPacket packet = 
-	    new DatagramPacket(buffer, buffer.length);
-	s.receive(packet);
-	
-	return packet;
+		DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+		s.receive(packet);
+		return packet;
     }
     
     public byte[] receiveData() throws IOException{
 
-	DatagramPacket packet = 
-	    new DatagramPacket(buffer, buffer.length);
-	s.receive(packet);
-	
-	return packet.getData();
+		DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+		s.receive(packet);
+		return packet.getData();
     }
 
-    public void send(InetAddress recv, int port,byte[] message)
-       throws IOException {
-      
-	//InetAddress recv = InetAddress.getByName(host);
-       DatagramPacket packet = 
-	   new DatagramPacket(message, message.length, recv, port);
-       s.send(packet);
+    public void send(InetAddress recv, int port,byte[] message) throws IOException {
+		//InetAddress recv = InetAddress.getByName(host);
+		DatagramPacket packet =
+	    new DatagramPacket(message, message.length, recv, port);
+        s.send(packet);
    }	
     
 }
--- VoiceProject/src/main/java/com/cn/mcc/utils/voice/udp/UDPSender.java
+++ VoiceProject/src/main/java/com/cn/mcc/utils/voice/udp/UDPSender.java
@@ -26,15 +26,84 @@ public class UDPSender implements Sender{
      * @param port    the listening port on the receiving host
      */
     public UDPSender(InetAddress address, int port) throws IOException{
-	toPort = port;
-	toAddress = address;
-	msg = new byte[CHUNCKED_SIZE];
-	buffer = new byte[2560];
-	s = new DatagramSocket();
+		toPort = port;
+		toAddress = address;
+		msg = new byte[CHUNCKED_SIZE];
+		buffer = new byte[2560];
+		s = new DatagramSocket();
         s.connect(toAddress, toPort);
     }
-    
 
+	public void sendByte(byte[] data) throws IOException{
+		int length = 0;
+
+		System.out.println(" -- Filename: "+theFile.getName());
+		System.out.println(" -- Bytes to send: "+data.length);
+		byte[] bytes = new byte[CHUNCKED_SIZE];
+		try {
+			while ((length = fileReader.read(bytes, 0, bytes.length)) > 0) {
+				int len = -1;
+				while ((len = fileReader.read(bytes)) != -1) {
+					if (len < CHUNCKED_SIZE) {
+						//send(Arrays.copyOfRange(bytes, 0, length));
+						send((theFile.getName()+"::"+Arrays.copyOfRange(bytes, 0, length)).getBytes());
+						break;
+					}
+					//send(bytes);
+				}
+				send((theFile.getName()+"::"+bytes).getBytes());
+				Thread.sleep(160);
+				//send((theFile.getName()+"::"+Arrays.copyOfRange(bytes, 0, fileLength)).getBytes());
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+/*
+		// 发送音频
+		byte[] bytes = new byte[CHUNCKED_SIZE];
+		try (RandomAccessFile raf = new RandomAccessFile(theFile.getName(), "r")) {
+			int len = -1;
+			while ((len = raf.read(bytes)) != -1) {
+				if (len < CHUNCKED_SIZE) {
+					//send(client, bytes = Arrays.copyOfRange(bytes, 0, len));
+					send((theFile.getName()+"::"+fileLength).getBytes());
+					break;
+				}
+
+				send((theFile.getName()+"::"+fileLength).getBytes());
+				// 每隔40毫秒发送一次数据
+				Thread.sleep(160);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}*/
+
+		// 1. Send the filename to the receiver
+		send((theFile.getName()+"::"+fileLength).getBytes());
+
+		// 2. Wait for a reply from the receiver
+		DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
+		s.receive(reply);
+		//initReply = (new String(reply.getData(), 0, reply.getLength()));
+
+
+		// 3. Send the content of the file
+		if (new String(reply.getData(), 0, reply.getLength()).equals("OK")) {
+			System.out.println("  -- Got OK from receiver - sending the file ");
+
+			while (currentPos<fileLength){
+				//System.out.println("Will read at pos: "+currentPos);
+				bytesRead = fileReader.read(msg);
+				send(msg);
+				//System.out.println("Bytes read: "+bytesRead);
+				currentPos = currentPos + bytesRead;
+			}
+			System.out.println("  -- File transfer complete...");
+		} else{
+			System.out.println("Recieved something other than OK... exiting");
+		}
+	}
     /**
      * Sends a file to the bound host.
      * Reads the contents of the specified file, and sends it via UDP to the host 
@@ -45,14 +114,13 @@ public class UDPSender implements Sender{
     public void sendFile(File theFile) throws IOException{
 		int length = 0;
 		// Init stuff
-	fileReader = new FileInputStream(theFile);
-	fileLength = fileReader.available();
+		fileReader = new FileInputStream(theFile);
+		fileLength = fileReader.available();
 	
-	System.out.println(" -- Filename: "+theFile.getName());
-	System.out.println(" -- Bytes to send: "+fileLength);
+		System.out.println(" -- Filename: "+theFile.getName());
+		System.out.println(" -- Bytes to send: "+fileLength);
 		byte[] bytes = new byte[CHUNCKED_SIZE];
 		try {
-
 			while ((length = fileReader.read(bytes, 0, bytes.length)) > 0) {
 				int len = -1;
 				while ((len = fileReader.read(bytes)) != -1) {
@@ -92,11 +160,11 @@ public class UDPSender implements Sender{
 		}*/
 
 	// 1. Send the filename to the receiver
-	send((theFile.getName()+"::"+fileLength).getBytes());
+		send((theFile.getName()+"::"+fileLength).getBytes());
 	
 	// 2. Wait for a reply from the receiver
-	DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
-	s.receive(reply);
+		DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
+		s.receive(reply);
 	//initReply = (new String(reply.getData(), 0, reply.getLength()));
 	
 	
@@ -119,8 +187,7 @@ public class UDPSender implements Sender{
     
 
     private void send(byte[] message) throws IOException {
-	DatagramPacket packet = 
-            new DatagramPacket(message, message.length);
-	s.send(packet);
+		DatagramPacket packet = new DatagramPacket(message, message.length);
+		s.send(packet);
     }	
 }
